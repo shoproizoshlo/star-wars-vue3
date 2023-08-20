7bc47c523dc83c1e10f0a9817806b5fba2d41c2a
shoproizoshlo <iamsusunny@gmail.com>
2023-08-20 17:09:44 +0200

    fixed search and preloader

--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -1,17 +1,18 @@
 <template>
-  <input v-model="searchTerm" @input="searchFilms" placeholder="Search for a film" class="ps-3" />
-
   <Loader v-if="loading" />
-  <div v-else class="row row-cols-1 row-cols-md-2 g-4">
-    <div class="col" v-for="film in filteredFilms" :key="film.id">
-      <div class="card">
-        <img src="/src/assets/icons/vader.png" class="card-img-top" alt="Dart Vader image" />
-        <div class="card-body">
-          <h5 class="card-title">{{ film.title }}</h5>
-          <p class="card-text">{{ film.opening_crawl }}</p>
-          <p class="card-text">Director – {{ film.director }}</p>
-          <p class="card-text">Producer – {{ film.producer }}</p>
-          <p class="card-text">Release date – {{ film.release_date }}</p>
+  <div v-else>
+    <input v-model="searchTerm" @input="searchFilms" placeholder="Search for a film" class="ps-3" />
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+      <div class="col" v-for="film in filteredFilms" :key="film.id">
+        <div class="card">
+          <img src="/src/assets/icons/vader.png" class="card-img-top" alt="Dart Vader image" />
+          <div class="card-body">
+            <h5 class="card-title">{{ film.title }}</h5>
+            <p class="card-text">{{ film.opening_crawl }}</p>
+            <p class="card-text">Director – {{ film.director }}</p>
+            <p class="card-text">Producer – {{ film.producer }}</p>
+            <p class="card-text">Release date – {{ film.release_date }}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -24,10 +25,8 @@ import swapiService from '@/services/swapiService'
 import type { Film } from '@/interfaces/FilmInterface'
 import Loader from './Loader.vue'
 
-const loading = ref(false)
-
 const films = ref<Film[]>([])
-
+const loading = ref(true)
 const searchTerm = ref('')
 
 const filteredFilms = computed(() => {
@@ -42,6 +41,8 @@ const searchFilms = async () => {
     films.value = response.data.results
   } catch (error) {
     console.error('Error fetching films:', error)
+  } finally {
+    loading.value = false
   }
 }
 
